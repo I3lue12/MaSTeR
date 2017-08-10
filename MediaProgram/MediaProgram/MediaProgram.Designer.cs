77e205a1866e89ac9ad8ab5410f7c69570416d72
brendon <brendon.blau@yahoo.com>
2017-08-10 12:17:02 -0700

    MediaProgram
    put media proggram on my laptop

---
+++ MediaProgram/MediaProgram/MediaProgram.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MediaProgram
+{
+    partial class MediaProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dropPlay = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.searchMedia = new System.Windows.Forms.Button();
+            this.DropMediaHere = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dropPlay
+            // 
+            this.dropPlay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dropPlay.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.dropPlay.Location = new System.Drawing.Point(392, 130);
+            this.dropPlay.Name = "dropPlay";
+            this.dropPlay.Size = new System.Drawing.Size(525, 377);
+            this.dropPlay.TabIndex = 0;
+            this.dropPlay.Paint += new System.Windows.Forms.PaintEventHandler(this.dropPlay_Paint);
+            // 
+            // searchMedia
+            // 
+            this.searchMedia.Location = new System.Drawing.Point(604, 534);
+            this.searchMedia.Name = "searchMedia";
+            this.searchMedia.Size = new System.Drawing.Size(100, 25);
+            this.searchMedia.TabIndex = 2;
+            this.searchMedia.Text = "Search Media";
+            this.searchMedia.UseVisualStyleBackColor = true;
+            this.searchMedia.Click += new System.EventHandler(this.searchMedia_Click);
+            // 
+            // DropMediaHere
+            // 
+            this.DropMediaHere.Location = new System.Drawing.Point(500, 84);
+            this.DropMediaHere.Name = "DropMediaHere";
+            this.DropMediaHere.Size = new System.Drawing.Size(300, 20);
+            this.DropMediaHere.TabIndex = 3;
+            this.DropMediaHere.Text = "Drag and drop  your media in the box!\r\n";
+            this.DropMediaHere.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DropMediaHere.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MediaProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::MediaProgram.Properties.Resources.grungy_blue_texture_by_waitq_d6u7g9q;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.DropMediaHere);
+            this.Controls.Add(this.searchMedia);
+            this.Controls.Add(this.dropPlay);
+            this.Name = "MediaProgram";
+            this.Text = "MediaProgram";
+            this.Load += new System.EventHandler(this.MediaProgram_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel dropPlay;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button searchMedia;
+        private System.Windows.Forms.TextBox DropMediaHere;
+    }
+}
+
---
+++ MediaProgram/MediaProgram/MediaProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaProgram
+{
+    public partial class MediaProgram : Form
+    {
+        public MediaProgram()
+            {
+                InitializeComponent();
+            }
+
+        private void MediaProgram_Load(object sender, EventArgs e)
+            {
+
+            }
+        private void searchMedia_Click(object sender, EventArgs e)
+            {
+                //Button for searching for the media on computer.
+            }
+
+        private void dropPlay_Paint(object sender, PaintEventArgs e) 
+            {
+                //User drags file or files here to be read, to play either a movie/picture/music and send them to there form
+
+            }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)//text box that tells the user what to do at start of screen to play media
+            {
+
+            }
+
+        
+    }
+}
