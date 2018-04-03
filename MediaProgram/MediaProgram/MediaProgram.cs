73cb45f4376d19470d2b591d0ef702444735f147
brendon <brendon.blau@yahoo.com>
2018-04-03 10:58:11 -0700

    PRG_321_Start
    starting prg 321 morning github

--- MediaProgram/MediaProgram/MediaProgram.cs
+++ MediaProgram/MediaProgram/MediaProgram.cs
@@ -12,61 +12,81 @@ namespace MediaProgram
 {
     public partial class MediaProgram : Form
     {
+		frmVideo Video;//mental note child?
+		
+		
+		public void Display()
+		{
+			Text = "hello " + Video.nameFrommom;
+		}
+
         public MediaProgram()
-            {
-                InitializeComponent();
-            }
+        {
+            InitializeComponent();
+        }
 
         private void Talk(string whatToSay)
         {
-			
-
 
         }
-            private void MediaProgram_Load(object sender, EventArgs e)
-            {
+        private void MediaProgram_Load(object sender, EventArgs e)
+        {
 
-            }
+        }
         private void searchMedia_Click(object sender, EventArgs e)
-            {
-                //Button for searching for the media on computer.
-                frmVideo Video = new frmVideo();
-                Video.Show();
-            }
+        {
+            //Button for searching for the media on computer.
+			Video = new frmVideo(this);
+				
+			Video.Show();	
+			this.Hide();
+				
+        }
 
         private void dropPlay_Paint(object sender, PaintEventArgs e) 
-            {
-                //User drags file or files here to be read, to play either a movie/picture/music and send them to there form
-
-            }
+        {
+            //User drags file or files here to be read, to play either a movie/picture/music and send them to there form
+			
+			//I want items to send to frmVideo ---> for now just do videos <-----
+			// I want to just send the videos. If the dragged files are not video 
+			// we will just ignore that and continue with the files that are video.
+        }
         
 
         private void textBox1_TextChanged(object sender, EventArgs e)//text box that tells the user what to do at start of screen to play media
-            {
+        {
 
-            }
+        }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)//not sure what this is..
-            {
+        {
 
-            }
+        }
 
         private void dropPlay_MouseEnter(object sender, EventArgs e)
-            {
-                Text = "Mouse just entered the Panel";
-                Cursor= Cursors.Cross;
-            }
+        {
+            Text = "Mouse just entered the Panel";
+            Cursor= Cursors.Cross;
+        }
 
         private void dropPlay_MouseLeave(object sender, EventArgs e)
-            {
-                Text = "Mouse just left";
-                Cursor = Cursors.Default;
-            }
+        {
+            Text = "Mouse just left";
+            Cursor = Cursors.Default;
+        }
 
         private void dropPlay_MouseMove(object sender, MouseEventArgs e)
-            {
-                Text = "X: " + e.X + ", Y: " + e.Y;
-            }
+        {
+            Text = "X: " + e.X + ", Y: " + e.Y;
+        }
+
+		private void dropPlay_DragDrop(object sender, DragEventArgs e)//--> check to see what this is, it is in the propertys and what is the difference
+																	  //    with this and double clicking the field itself.
+		{
+			//I want items to send to frmVideo ---> for now just do videos <-----
+ 			// I want to just send the videos. If the dragged files are not video 
+			// we will just ignore that and continue with the files that are video.
+		}
 
         
     }
--- MediaProgram/MediaProgram/frmVideo.Designer.cs
+++ MediaProgram/MediaProgram/frmVideo.Designer.cs
@@ -28,33 +28,130 @@
         /// </summary>
         private void InitializeComponent()
             {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "video form";
-            // 
-            // frmVideo
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 330);
-            this.Controls.Add(this.label1);
-            this.Name = "frmVideo";
-            this.Text = "frmVideo";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVideo));
+			this.label1 = new System.Windows.Forms.Label();
+			this.frmMediaProgram = new System.Windows.Forms.Button();
+			this.Setting = new System.Windows.Forms.Button();
+			this.mediaNav = new System.Windows.Forms.ComboBox();
+			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+			this.videoPlayList = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.removebtnVideo = new System.Windows.Forms.Button();
+			this.addbtnVideo = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(501, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "video form";
+			// 
+			// frmMediaProgram
+			// 
+			this.frmMediaProgram.Location = new System.Drawing.Point(12, 205);
+			this.frmMediaProgram.Name = "frmMediaProgram";
+			this.frmMediaProgram.Size = new System.Drawing.Size(75, 23);
+			this.frmMediaProgram.TabIndex = 1;
+			this.frmMediaProgram.Text = "Home";
+			this.frmMediaProgram.UseVisualStyleBackColor = true;
+			this.frmMediaProgram.Click += new System.EventHandler(this.frmMediaProgram_Click);
+			// 
+			// Setting
+			// 
+			this.Setting.Location = new System.Drawing.Point(12, 270);
+			this.Setting.Name = "Setting";
+			this.Setting.Size = new System.Drawing.Size(75, 23);
+			this.Setting.TabIndex = 2;
+			this.Setting.Text = "Setting";
+			this.Setting.UseVisualStyleBackColor = true;
+			this.Setting.Click += new System.EventHandler(this.Setting_Click);
+			// 
+			// mediaNav
+			// 
+			this.mediaNav.FormattingEnabled = true;
+			this.mediaNav.Items.AddRange(new object[] {
+            "Music",
+            "Photos",
+            "Settings"});
+			this.mediaNav.Location = new System.Drawing.Point(12, 12);
+			this.mediaNav.Name = "mediaNav";
+			this.mediaNav.Size = new System.Drawing.Size(121, 21);
+			this.mediaNav.TabIndex = 4;
+			this.mediaNav.SelectedIndexChanged += new System.EventHandler(this.mediaNav_SelectedIndexChanged);
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			this.axWindowsMediaPlayer1.Enabled = true;
+			this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(221, 39);
+			this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+			this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(599, 394);
+			this.axWindowsMediaPlayer1.TabIndex = 5;
+			// 
+			// videoPlayList
+			// 
+			this.videoPlayList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+			this.videoPlayList.Location = new System.Drawing.Point(826, 59);
+			this.videoPlayList.Name = "videoPlayList";
+			this.videoPlayList.Size = new System.Drawing.Size(190, 325);
+			this.videoPlayList.TabIndex = 6;
+			this.videoPlayList.UseCompatibleStateImageBehavior = false;
+			this.videoPlayList.SelectedIndexChanged += new System.EventHandler(this.videoPlayList_SelectedIndexChanged);
+			// 
+			// removebtnVideo
+			// 
+			this.removebtnVideo.Location = new System.Drawing.Point(826, 390);
+			this.removebtnVideo.Name = "removebtnVideo";
+			this.removebtnVideo.Size = new System.Drawing.Size(86, 27);
+			this.removebtnVideo.TabIndex = 7;
+			this.removebtnVideo.Text = "Remove";
+			this.removebtnVideo.UseVisualStyleBackColor = true;
+			// 
+			// addbtnVideo
+			// 
+			this.addbtnVideo.Location = new System.Drawing.Point(930, 390);
+			this.addbtnVideo.Name = "addbtnVideo";
+			this.addbtnVideo.Size = new System.Drawing.Size(86, 27);
+			this.addbtnVideo.TabIndex = 8;
+			this.addbtnVideo.Text = "Add";
+			this.addbtnVideo.UseVisualStyleBackColor = true;
+			// 
+			// frmVideo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1028, 445);
+			this.Controls.Add(this.addbtnVideo);
+			this.Controls.Add(this.removebtnVideo);
+			this.Controls.Add(this.videoPlayList);
+			this.Controls.Add(this.axWindowsMediaPlayer1);
+			this.Controls.Add(this.mediaNav);
+			this.Controls.Add(this.Setting);
+			this.Controls.Add(this.frmMediaProgram);
+			this.Controls.Add(this.label1);
+			this.Name = "frmVideo";
+			this.Text = "frmVideo";
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
             }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button frmMediaProgram;
+		private System.Windows.Forms.Button Setting;
+		private System.Windows.Forms.ComboBox mediaNav;
+		private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+		private System.Windows.Forms.ListView videoPlayList;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.Button removebtnVideo;
+		private System.Windows.Forms.Button addbtnVideo;
         }
     }
