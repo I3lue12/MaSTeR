81d087d977ee81980f026f4acac3a7b27ba30e37
brendon <brendon.blau@yahoo.com>
2018-04-25 16:27:04 -0700

    working on week 4

---
+++ PRG_321_Morning/Week_4/Week4/Week4/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>          <!--<asp:Button ID="Button1" runat="server" Text="Button" /> -->
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Enter In Somthing:     "></asp:Label>
+        <asp:TextBox ID="txtBoxItemEnter" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnListAdd" runat="server" Text="Add To List" OnClick="Button_Click" />
+        <br />
+        <br />
+        <asp:ListBox ID="lstBoxItemsAdded" runat="server" Height="103px" Width="194px" OnInit="SelectedText" OnSelectedIndexChanged="OnSelectedChange" AutoPostBack="true"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Label ID="lbl_ListBoxSelection" runat="server" Text="details of selection" ViewStateMode="Enabled"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+&nbsp;&nbsp;
+    </form>
+</body>
+</html>
---
+++ PRG_321_Morning/Week_4/Week4/Week4/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{   public string select;
+    protected void Page_PreInit(object sender, EventArgs e)       /*1*/{ }
+    protected void Page_Init(object sender, EventArgs e)          /*2*/{ /*Response.Write("<h1 style=\"color: blue;\">My dynamic Web</h1>"); write c# code to write html*/}
+    protected void Page_InitComplete(object sender, EventArgs e)  /*3*/{ }
+    protected void Page_PreLoad(object sender, EventArgs e)       /*4*/{ }
+    protected void Page_Load(object sender, EventArgs e)          /*5*/{/*Button1.BackColor = System.Drawing.Color.Blue;*/}
+
+    protected void Page_Control_events(object sender, EventArgs e)/*6*/{ }
+
+    
+
+
+    protected void OnSelectedChange(object sender, EventArgs e)//selected event arg.
+    {
+        //select = lstBoxItemsAdded.SelectedItem.ToString();    
+        lbl_ListBoxSelection.Text = lstBoxItemsAdded.SelectedItem.Text;
+        //lbl_ListBoxSelection.Text = select;
+        
+    }
+    
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        string content = txtBoxItemEnter.Text;
+        if (string.Empty == content)
+        {
+            return;
+        }        
+        lstBoxItemsAdded.Items.Add(content);
+        //select = content;
+        txtBoxItemEnter.Text = "";        
+        
+    }
+    
+
+   
+    protected void Page_LoadComplete(object sender, EventArgs e)      /*7*/ { }
+    protected void Page_PreRender(object sender, EventArgs e)         /*8*/ { }
+    protected void Page_PreRenderComplete(object sender, EventArgs e) /*9*/ { }
+    protected void Page_SaveStateComplete(object sender, EventArgs e)/*10*/ { }
+    protected void Page_Render(object sender, EventArgs e)           /*11*/ { }
+    protected void Page_Unload(object sender, EventArgs e)           /*12*/ { }
+
+
+    protected void SelectedText(object sender, EventArgs e)                 { }
+
+   
+}
