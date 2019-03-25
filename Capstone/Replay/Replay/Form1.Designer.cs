189cc7afae955cfd2c32c3081b61688c596e85c9
Brendon Blau <brendon.blau@yahoo.com>
2019-03-24 22:35:14 -0700

    2nd half proj

---
+++ Capstone/Replay/Replay/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Replay
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lbViewFiles = new System.Windows.Forms.ListBox();
+			this.btnOpenFile = new System.Windows.Forms.Button();
+			this.btnPlay = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lbViewFiles
+			// 
+			this.lbViewFiles.FormattingEnabled = true;
+			this.lbViewFiles.Location = new System.Drawing.Point(309, 75);
+			this.lbViewFiles.Name = "lbViewFiles";
+			this.lbViewFiles.ScrollAlwaysVisible = true;
+			this.lbViewFiles.Size = new System.Drawing.Size(333, 277);
+			this.lbViewFiles.TabIndex = 0;
+			this.lbViewFiles.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbViewFiles_DrawItem);
+			this.lbViewFiles.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.lbViewFiles_MeasureItem);
+			this.lbViewFiles.SelectedIndexChanged += new System.EventHandler(this.lbViewFiles_SelectedIndexChanged);
+			// 
+			// btnOpenFile
+			// 
+			this.btnOpenFile.Location = new System.Drawing.Point(309, 40);
+			this.btnOpenFile.Name = "btnOpenFile";
+			this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+			this.btnOpenFile.TabIndex = 1;
+			this.btnOpenFile.Text = "Open File";
+			this.btnOpenFile.UseVisualStyleBackColor = true;
+			this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+			// 
+			// btnPlay
+			// 
+			this.btnPlay.Location = new System.Drawing.Point(309, 359);
+			this.btnPlay.Name = "btnPlay";
+			this.btnPlay.Size = new System.Drawing.Size(75, 23);
+			this.btnPlay.TabIndex = 2;
+			this.btnPlay.Text = "PLAY";
+			this.btnPlay.UseVisualStyleBackColor = true;
+			this.btnPlay.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnPlay);
+			this.Controls.Add(this.btnOpenFile);
+			this.Controls.Add(this.lbViewFiles);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lbViewFiles;
+		private System.Windows.Forms.Button btnOpenFile;
+		private System.Windows.Forms.Button btnPlay;
+	}
+}
+
---
+++ Capstone/Replay/Replay/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Drawing.Text;
+
+namespace Replay
+{
+	public partial class Form1 : Form
+	{
+		Label lbInstruct = new Label();
+		string filePath;
+
+		public Form1()
+		{
+			InitializeComponent();
+			string somethign = "";
+			lbViewFiles.Items.Add(somethign);
+			lbViewFiles.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+			this.Controls.Add(lbInstruct);
+
+			lbInstruct.Show();
+			lbInstruct.Text = "asdfasdf";
+			lbInstruct.Left = this.Width / 2;
+			lbInstruct.Top = this.Height / 2;		
+			//lbInstruct.Visible = false;
+			lbInstruct.Visible = true; //DELETE
+			lbInstruct.Left = 10;
+			lbInstruct.Top = 10;
+			lbInstruct.Width = 1000; //bain of my exISTANCE
+			
+		}
+
+		private void btnOpenFile_Click(object sender, EventArgs e)
+		{
+			//OpenFileDialog openFile = new OpenFileDialog();
+
+			//openFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+			using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+			{
+				if (fbd.ShowDialog() == DialogResult.OK)
+				{
+					lbViewFiles.Items.Clear();
+					string[] files = Directory.GetFiles(fbd.SelectedPath);
+					string[] direct = Directory.GetDirectories(fbd.SelectedPath);
+					List<string> paths = new List<string>();
+					foreach (string s in files)
+					{
+						if (Path.GetFileName(s).Contains(".txt"))
+						{
+							lbViewFiles.Items.Add(Path.GetFileName(s));
+							paths.Add(Path.GetFileName(s));	  //adds all txt files in directory to list.
+						}
+					}
+					foreach (string d in direct)
+					{
+						lbViewFiles.Items.Add(Path.GetDirectoryName(d)); //adds to view the direcotrys in selected direcotory.
+					}
+				}
+			}
+
+
+			//using (OpenFileDialog openFile = new OpenFileDialog())
+			//{
+
+			//	openFile.InitialDirectory = "c:\\";
+
+			//	if (openFile.ShowDialog() == DialogResult.OK)
+			//	{
+			//		string fileLoc = openFile.FileName;
+			//		string direct = Path.GetDirectoryName(fileLoc);
+			//		//grab file location
+			//		lbViewFiles.Items.Add(direct);
+
+			//	}
+			//}
+			
+		}
+
+		private void lbViewFiles_MeasureItem(object sender, MeasureItemEventArgs e)
+		{
+			e.ItemHeight = (int)e.Graphics.MeasureString(lbViewFiles.Items[e.Index].ToString(), lbViewFiles.Font, lbViewFiles.Width).Height;
+		}
+	
+
+		private void lbViewFiles_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			e.DrawBackground();
+			e.DrawFocusRectangle();
+			e.Graphics.DrawString(lbViewFiles.Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int exTimer = 10;
+			lbViewFiles.Hide();
+			btnOpenFile.Hide();
+
+			while (exTimer > 0)
+			{
+				lbInstruct.Visible = true;
+				lbInstruct.Text = string.Format("Dont touch program: {0}", exTimer);
+				this.Update(); //repaint.
+				Thread.Sleep(1000);
+				exTimer -= 1;
+			}
+			this.Hide();
+			//TODO: Run grab file.
+			//TODO: Run decrypt
+			//TODO: Run playfunction
+			
+
+
+			//bring back the program
+			this.Show();
+			lbViewFiles.Show();
+			btnOpenFile.Show();
+			this.Visible = true;
+		}
+
+		private void lbViewFiles_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//filePath = lbViewFiles.GetItemText(lbViewFiles.SelectedItem);
+			
+			filePath = lbViewFiles.SelectedItem.ToString();
+			string ccc="";
+			foreach(char c in filePath)
+			{
+				ccc += c.ToString();
+			}
+			MessageBox.Show(filePath);
+			//lbInstruct.Text = "Clicked";
+			lbInstruct.Text = string.Format("Play: {0}" , filePath.ToString());
+			//lbInstruct.BringToFront();
+			//this.Update();
+			
+		}
+	}
+}
