7e9acb4ec1a9158e22bd18e16f06cfa27e63e64e
Brendon Blau <brendon.blau@yahoo.com>
2019-06-12 17:04:49 -0700

    update
    updateing github

---
+++ ReadFileText/ReadFileText/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ReadFileText
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnFindFile = new System.Windows.Forms.Button();
+			this.lbViewFileContents = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// btnFindFile
+			// 
+			this.btnFindFile.Location = new System.Drawing.Point(12, 13);
+			this.btnFindFile.Name = "btnFindFile";
+			this.btnFindFile.Size = new System.Drawing.Size(75, 23);
+			this.btnFindFile.TabIndex = 0;
+			this.btnFindFile.Text = "Find File";
+			this.btnFindFile.UseVisualStyleBackColor = true;
+			this.btnFindFile.Click += new System.EventHandler(this.btnFindFile_Click);
+			// 
+			// lbViewFileContents
+			// 
+			this.lbViewFileContents.FormattingEnabled = true;
+			this.lbViewFileContents.Location = new System.Drawing.Point(114, 12);
+			this.lbViewFileContents.Name = "lbViewFileContents";
+			this.lbViewFileContents.Size = new System.Drawing.Size(566, 420);
+			this.lbViewFileContents.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lbViewFileContents);
+			this.Controls.Add(this.btnFindFile);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnFindFile;
+		private System.Windows.Forms.ListBox lbViewFileContents;
+	}
+}
+
---
+++ ReadFileText/ReadFileText/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace ReadFileText
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnFindFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			DialogResult dr = new DialogResult();
+			dr = ofd.ShowDialog();
+			if(dr ==  DialogResult.OK)
+			{
+				lbViewFileContents.Items.Add(ofd.FileName);
+				FileInfo fi = new FileInfo(ofd.FileName);
+				using (FileStream fs = new File
+					Stream(fi.FullName, FileMode.Open))
+				{
+					fs.Read()
+				}
+			}
+		}
+	}
+}
